f40deb54ce8afddcdfd8c10271613ccb2e25b93f
EranMani <eranmanim@gmail.com>
2026-02-25 16:11:26 +0200

    refactor: run load_document function and test both pdf and txt sample files

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from ingestion.loaders import load_document
+from pathlib import Path
+
+working_dir = Path(__file__).resolve().parent
+
 def main():
-    print("Hello from rag-doc-chat!")
+    docs = load_document(file_path=rf"{working_dir}\data\sample.pdf", filename="sample.pdf")
+    print(len(docs), docs[0].metadata)
+    print(docs[0].page_content)
+
+    with open(rf"{working_dir}\data\sample.txt", "rb") as f:
+        data = f.read()
+
+    docs = load_document(file_bytes=data, filename="sample.txt")
+    print(len(docs), docs[0].metadata)
+    print(docs[0].page_content)
 
 
 if __name__ == "__main__":
