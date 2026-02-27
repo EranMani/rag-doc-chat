93bb1ec98d6ff8d63c25d24d3233361cbdab8efc
EranMani <eranmanim@gmail.com>
2026-02-27 09:51:15 +0200

    internal: provide ingest document stream as public entrypoint

--- ingestion/__init__.py
+++ ingestion/__init__.py
@@ -1,4 +1,4 @@
-from .ingest import ingest_document
+from .ingest import ingest_document, ingest_document_stream
 
 # Make ingest_document the only public entrypoint from the ingest module
-__all__ = ["ingest_document"]
+__all__ = ["ingest_document", "ingest_document_stream"]
