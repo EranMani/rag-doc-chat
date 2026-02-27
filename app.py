d2878c3695d520ab1746ba730835bc03c22ecafe
EranMani <eranmanim@gmail.com>
2026-02-27 09:15:46 +0200

    feat: add slider as progress bar for the upload and summary function output

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import gradio as gr
 from pathlib import Path
 from ingestion import ingest_document
+import time
 
 def process_document_upload(file):
     if file is None:
@@ -9,11 +10,14 @@ def process_document_upload(file):
     # Make sure the file from gradio come as a string. if not, get the first element of the list.
     path = file if isinstance(file, str) else file[0]
     filename = Path(path).name
+    yield 10, "⏳ Loading and processing document..."
     try:
         summary = ingest_document(file_path=path, filename=filename)
-        return f"Summary: {summary}"
+        yield 100, summary if summary else "No summary available"
+    except ValueError as e:
+        yield 100, f"Invalid file format: {e}"
     except Exception as e:
-        return f"Error processing file: {e}"
+        yield 100, f"Error processing file: {e}"
 
 
 # Create a gradio interface with a custom layout using Blocks
@@ -23,18 +27,21 @@ with gr.Blocks(title="RAG Doc Chat") as demo:
     with gr.Row():
         with gr.Column():
             gr.Markdown("### Upload & Summary")
+            progress_bar = gr.Progress()
             # file input is the component whole value (the path) is used as input when the button is clicked
             # setting type to filepath to tell gradio that it should pass the path to the uploaded file as a string to the function, not the file bytes
             file_input = gr.File(label="Upload Document", file_types=[".pdf", ".csv", ".txt", ".md"], type="filepath")
             process_btn = gr.Button("Process")
             # A markdown area where the summary will be displayed
             # whatever the process document upload function returnes, will be displayed in this area
+            progress_bar = gr.Slider(0, 100, value=0, label="Progress", interactive=False)
             summary_out = gr.Markdown(label="Document Summary")
         with gr.Column():
             gr.Markdown("## Chat (Phase 5)")
             gr.Markdown("Ask questions about your documents here.")
 
     # wires the process button to logic
-    process_btn.click(fn=process_document_upload, inputs=file_input, outputs=summary_out)
+    process_btn.click(fn=process_document_upload, inputs=file_input, outputs=[progress_bar, summary_out])
 
+demo.queue()
 demo.launch()
